1131890a71bda80b8146d9b8e0f748271761be1d
n0mi <nomibzu@gmail.com>
2014-06-09 12:08:47 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomiapp::Application.config.secret_key_base = 'ec9b4ebd02ba2e44e6dd13c1b176b44f5dd6545d9cb55d29e59131a098bb470ff28871d96c73cd5a6078f398077a44689850646206a9a0e7c84d8a5691d75286'
